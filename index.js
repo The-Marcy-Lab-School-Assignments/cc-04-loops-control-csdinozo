a88130cbb0ff5c77b360a1aa0e8ac97353220330
Catalina Dinozo <csdinozo@gmail.com>
2024-10-08 10:21:06 -0400

    p1 and p2 count from one and every odd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Write your solutions in this file, don't forget to test your functions.
+
+const countFromOne = (n) => {
+	for (let n = 1; n <= val; n++) console.log(n);
+}
+
+const countEveryOdd = (val) => {
+	for (let n = 1; n <= val; n++) if (n % 2 != 0) console.log(n);
+}
