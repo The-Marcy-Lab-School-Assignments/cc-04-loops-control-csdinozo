3b58e35ebffadbd616c2351fafd30ed5cb43b676
Catalina Dinozo <csdinozo@gmail.com>
2024-10-08 10:58:36 -0400

    revised p5 given start of range

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const betweenFiveAndTwenty = n => {
 const sumOfThreeOrFive = () => {
 	let sum = 0;
 
-	for (let i = 0; i < 1000; i++) {
+	for (let i = 1; i < 1000; i++) {
 		if (i % 3 == 0 || i % 5 == 0) {
 			sum += i;
 		}
