f94a925e7fa2d9275d5a394c45a311e1a765b96e
Catalina Dinozo <csdinozo@gmail.com>
2024-10-08 10:24:09 -0400

    p3 is negative

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const countFromOne = (n) => {
 const countEveryOdd = (val) => {
 	for (let n = 1; n <= val; n++) if (n % 2 != 0) console.log(n);
 }
+
+const isNegative = (n) => {
+	if (n < 0) return true;
+	else if (n > 0) return false;
+	// zero is neither positive nor negative
+	return null;
+}
