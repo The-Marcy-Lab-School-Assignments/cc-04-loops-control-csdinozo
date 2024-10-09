500b401d22101911aa98a65ea3b7be9ea562f9f8
Catalina Dinozo <csdinozo@gmail.com>
2024-10-09 10:12:34 -0400

    tested and revised

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 //Write your solutions in this file, don't forget to test your functions.
 
 // p1
-const countFromOne = n => {
+const countFromOne = val => {
 	for (let n = 1; n <= val; n++) console.log(n);
 }
+// countFromOne(10);
 
 // p2
 const countEveryOdd = val => {
 	for (let n = 1; n <= val; n++) if (n % 2 != 0) console.log(n);
 }
+// countEveryOdd(10);
 
 // p3
 const isNegative = n => {
 	return n < 0; // assume false when n === 0 since zero is not negative
 }
+// console.log(isNegative(1));
+// console.log(isNegative(-1));
 
 // p4
 const betweenFiveAndTwenty = n => {
 	return n >= 5 && n <= 20;
 }
+// console.log(betweenFiveAndTwenty(100));
+// console.log(betweenFiveAndTwenty(15));
 
 // p5
 const sumOfThreeOrFive = () => {
@@ -32,12 +38,15 @@ const sumOfThreeOrFive = () => {
 
 	return sum;
 }
+// console.log(sumOfThreeOrFive());
 
 // p6
 const isAllLowerCase = s => {
-	for (let i = 0; i < str.length; i++) if (s[i].toUpperCase() === s[i].toUpperCase()) return false;
+	for (let i = 0; i < s.length; i++) if (s[i].toUpperCase() === s[i]) return false;
 	return true;
 }
+// console.log(isAllLowerCase('hEllo'));
+// console.log(isAllLowerCase('hello'));
 
 // b1
 const countMultiplesOfFive = a => {
@@ -45,4 +54,5 @@ const countMultiplesOfFive = a => {
 	for (i = 0; i < a.length; i++) if (a[i] % 5 == 0) n++;
 	return n;
 }
+// console.log(countMultiplesOfFive([5,10,12]));
 
